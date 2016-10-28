7468364e55e355191a444d437e0e0252742d8493
Maximilian Bragazzi Ihrén <bragux@hotmail.com>
2016-10-28 17:43:28 +0200

    Reads from file, writes to file, can open dir, cool icon

--- TrayBot/TrayBot.cpp
+++ TrayBot/TrayBot.cpp
@@ -1,6 +1,13 @@
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <shellapi.h>
-#include <stdexcept>
+#include <CommCtrl.h>
+#include <Shlobj.h>
+
+#include <string>
+#include <tuple>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 #define NAM_HOTKEY_ID 1
 #define NAM_TRAYICON_ID 0
@@ -12,28 +19,123 @@
 
 #define NAM_EDIT_INPUT 0
 #define NAM_BTN_ADDNEW 1
+#define NAM_EDIT_CREATE 2
+
+enum class ACTION
+{
+	OPENFOLDER = 6000,
+	SETCLIPBOARD = 6001
+};
+
+//                           NAME          ACTION  INPUT
+#define NAM_ENTRY std::tuple<std::wstring, ACTION, std::wstring>
 
 HWND createNewHWND;
 HWND inputHWND;
+
+HWND createNewHWND_input;
+HWND createNewHWND_dropdown;
 
 HWND inputHWND_input;
 WNDPROC oldINPUTProc;
 
 HICON gIcon;
 NOTIFYICONDATA gNid;
-HMENU gMenu;
+HMENU gTrayMenu;
+HMENU gDropDownMenu;
 bool gDoubleClick;
+
+std::vector<NAM_ENTRY> gEntries;
+
+void _SHOW(HWND const& hwnd)
+{
+	SetForegroundWindow(hwnd);
+	ShowWindow(hwnd, SW_SHOW);
+}
+
+void _HIDE(HWND const& hwnd)
+{
+	ShowWindow(hwnd, SW_HIDE);
+}
+
+void showNotification(std::wstring const& title, std::wstring const& msg)
+{
+	gNid.uFlags = NIF_INFO;
+	wcscpy_s(gNid.szInfoTitle, title.c_str());
+	wcscpy_s(gNid.szInfo, msg.c_str());
+
+	Shell_NotifyIcon(NIM_MODIFY, &gNid);
+}
+
+void addNewEntry(std::wstring const& name, ACTION const& action, std::wstring const& input)
+{
+	std::size_t found = name.find('|');
+	if (found != std::string::npos) {
+		showNotification(TEXT("Error"), TEXT("Values can not contain the character '|'"));
+		return;
+	}
+
+	found = input.find('|');
+	if (found != std::string::npos) {
+		showNotification(TEXT("Error"), TEXT("Values can not contain the character '|'"));
+		return;
+	}
+
+	for (NAM_ENTRY const& ne : gEntries) {
+		if (std::get<0>(ne) == name) {
+			showNotification(TEXT("Error"), TEXT("Value ") + name + TEXT(" already exists!"));
+			return;
+		}
+	}
+
+	// Everything is fine
+	gEntries.push_back(NAM_ENTRY(name, action, input));
+
+	std::wofstream ofs(TEXT("data.txt"), std::ios::app);
+	ofs << name << '|' << int(action) << '|' << input << std::endl;
+	ofs.close();
+
+	_HIDE(createNewHWND);
+
+	showNotification(TEXT("Success"), TEXT("Added new value '") + name + TEXT("'"));
+}
+
+void executeEntry(NAM_ENTRY ne)
+{
+	switch (std::get<1>(ne)) {
+	case ACTION::OPENFOLDER: {
+		ShellExecute(NULL, TEXT("explore"), std::get<2>(ne).c_str(), NULL, NULL, SW_SHOW);
+	}
+		break;
+	case ACTION::SETCLIPBOARD: {
+		int len = std::get<2>(ne).length();
+		HGLOBAL newClip = GlobalAlloc(GMEM_MOVEABLE, len);
+		memcpy(GlobalLock(newClip), std::get<2>(ne).c_str(), len);
+		GlobalUnlock(newClip);
+
+		OpenClipboard(0);
+		EmptyClipboard();
+		SetClipboardData(CF_TEXT, newClip);
+		CloseClipboard();
+	}
+		break;
+	}
+}
 
 void parseInput(std::wstring const& str)
 {
-	std::string str1(str.begin(), str.end());
-	MessageBox(inputHWND, TEXT("YOU INPUTTED SOMETHING!"), NULL, 0);
+	for (NAM_ENTRY const& ne : gEntries) {
+		if (std::get<0>(ne) == str) {
+			executeEntry(ne);
+		}
+	}
 }
 
 void centerWindows()
 {
 	RECT rect;
 	GetWindowRect(createNewHWND, &rect);
+	//SetWindowLong(createNewHWND, GWL_STYLE, GetWindowLong(createNewHWND, GWL_STYLE) && !WS_BORDER && !WS_SIZEBOX && !WS_DLGFRAME);
 	SetWindowPos(createNewHWND, 0, (GetSystemMetrics(SM_CXSCREEN) - rect.right) / 2, (GetSystemMetrics(SM_CYSCREEN) - rect.bottom) / 2, rect.right, rect.bottom, 0);
 
 	GetWindowRect(inputHWND, &rect);
@@ -43,28 +145,29 @@ void centerWindows()
 
 void menuItems()
 {
-	AppendMenu(gMenu, MF_STRING, NAM_TRAY_SHOWWINDOW, TEXT("Show Input"));
-	AppendMenu(gMenu, MF_STRING, NAM_TRAY_ADDNEW, TEXT("Add New"));
-	AppendMenu(gMenu, MF_SEPARATOR, 0, NULL);
-	AppendMenu(gMenu, MF_STRING, NAM_TRAY_EXIT, TEXT("Quit"));
+	AppendMenu(gTrayMenu, MF_STRING, NAM_TRAY_SHOWWINDOW, TEXT("Show Input"));
+	AppendMenu(gTrayMenu, MF_STRING, NAM_TRAY_ADDNEW, TEXT("Add New"));
+	AppendMenu(gTrayMenu, MF_SEPARATOR, 0, NULL);
+	AppendMenu(gTrayMenu, MF_STRING, NAM_TRAY_EXIT, TEXT("Quit"));
 }
 
-LRESULT CALLBACK newEdit(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+LRESULT CALLBACK newINPUTEdit(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg) {
 	case WM_KILLFOCUS:
-		ShowWindow(inputHWND, SW_HIDE);
-		break;
+		_HIDE(inputHWND);
+		return 0;
 	case WM_KEYDOWN:
 		switch (wparam) {
 		case VK_ESCAPE:
-			ShowWindow(inputHWND, SW_HIDE);
+			_HIDE(inputHWND);
 			return 0;
 		case VK_RETURN:
 			TCHAR buffer[1024];
 			GetWindowText(inputHWND_input, buffer, 1024);
 			parseInput(std::wstring(buffer));
 			SetWindowText(inputHWND_input, TEXT(""));
+			_HIDE(inputHWND);
 			return 0;
 		}
 	default:
@@ -72,41 +175,74 @@ LRESULT CALLBACK newEdit(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	}
 }
 
+void dropdownitems()
+{
+	SendMessage(createNewHWND_dropdown, CB_ADDSTRING, 0, (LPARAM)TEXT("Open Folder"));
+	SendMessage(createNewHWND_dropdown, CB_ADDSTRING, 0, (LPARAM)TEXT("Set Clipboard"));
+
+	// Set index of intially shown item
+	SendMessage(createNewHWND_dropdown, CB_SETCURSEL, 0, 0);
+}
+
 void windowStyles()
 {
 	// Input
-	inputHWND_input = CreateWindow(TEXT("edit"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 10, 10, 300, 50, inputHWND, (HMENU) NAM_EDIT_INPUT, NULL, NULL);
+	inputHWND_input = CreateWindow(TEXT("edit"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER, 10, 10, 300, 50, inputHWND, (HMENU) NAM_EDIT_INPUT, NULL, NULL);
 	HFONT f = CreateFont(45, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, TEXT("Arial"));
 	SendMessage(inputHWND_input, WM_SETFONT, WPARAM(f), TRUE);
-	oldINPUTProc = (WNDPROC) SetWindowLongPtr(inputHWND_input, GWLP_WNDPROC, (LONG_PTR) newEdit);
+	oldINPUTProc = (WNDPROC) SetWindowLongPtr(inputHWND_input, GWLP_WNDPROC, (LONG_PTR) newINPUTEdit);
 
-	// Create New
-	CreateWindow(TEXT("button"), TEXT("Add New"), WS_VISIBLE | WS_CHILD, 10, 10, 80, 25, createNewHWND, (HMENU) NAM_BTN_ADDNEW, NULL, NULL);
+	// CREATE NEW
+	// "New Input" text
+	CreateWindow(TEXT("STATIC"), TEXT("New input:"), WS_VISIBLE | WS_CHILD, 10, 10, 100, 20, createNewHWND, NULL, NULL, NULL);
+
+	// Input box
+	createNewHWND_input = CreateWindow(TEXT("edit"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP, 10, 30, 300, 50, createNewHWND, (HMENU) NAM_EDIT_CREATE, NULL, NULL);
+	SendMessage(createNewHWND_input, WM_SETFONT, WPARAM(f), TRUE);
+
+	// Dropdown menu
+	createNewHWND_dropdown = CreateWindow(TEXT("COMBOBOX"), NULL, WS_CHILD | WS_VISIBLE | WS_OVERLAPPED | WS_TABSTOP | CBS_DROPDOWNLIST, 10, 90, 300, 100, createNewHWND, NULL, NULL, NULL);
+	dropdownitems();
+
+	// Ok button
+	CreateWindow(TEXT("button"), TEXT("Add New"), WS_VISIBLE | WS_CHILD | WS_TABSTOP, 10, 120, 300, 25, createNewHWND, (HMENU) NAM_BTN_ADDNEW, NULL, NULL);
+}
+
+std::wstring selectFolder()
+{
+	BROWSEINFO bi = { 0 };
+	bi.lpszTitle = TEXT("Select folder to open");
+	bi.ulFlags = BIF_RETURNONLYFSDIRS;
+	bi.lpfn = NULL;
+
+	LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
+	if (pidl == 0) { return NULL; }
+
+	TCHAR pathBuffer[MAX_PATH];
+	SHGetPathFromIDList(pidl, pathBuffer);
+
+	IMalloc* imalloc;
+	if (SUCCEEDED(SHGetMalloc(&imalloc))) {
+		imalloc->Free(pidl);
+		imalloc->Release();
+	}
+
+	return pathBuffer;
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg)
 	{
-	// Window created
-	case WM_CREATE:
-		// Create menu for tray icon
-		gMenu = CreatePopupMenu();
-		menuItems();
-
-		// Create window buttons
-		windowStyles();
-		break;
 	// Hotkey call
 	case WM_HOTKEY:
-		SetForegroundWindow(inputHWND);
-		ShowWindow(inputHWND, SW_SHOW);
+		_SHOW(inputHWND);
 		SetFocus(inputHWND_input);
 		break;
 	// X or ALT+F4
 	case WM_CLOSE:
-		ShowWindow(inputHWND, SW_HIDE);
-		ShowWindow(createNewHWND, SW_HIDE);
+		_HIDE(inputHWND);
+		_HIDE(createNewHWND);
 		break;
 	// Window is being destroyed
 	case WM_DESTROY:
@@ -121,7 +257,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	case WM_COMMAND:
 		switch (LOWORD(wparam)) {
 		case NAM_BTN_ADDNEW:
+			int index = SendMessage(createNewHWND_dropdown, CB_GETCURSEL, 0, 0) + 6000;
+			switch (index) {
+			case int(ACTION::OPENFOLDER) : {
+				std::wstring path = selectFolder();
 
+				TCHAR buffer[1024];
+				GetWindowText(createNewHWND_input, buffer, 1024);
+				SetWindowText(createNewHWND_input, TEXT(""));
+				addNewEntry(std::wstring(buffer), ACTION::OPENFOLDER, path);
+			}
+				break;
+			case int(ACTION::SETCLIPBOARD) :
+				MessageBox(NULL, TEXT("SET CLIPBOARD"), 0, 0);
+				break;
+			}
 			break;
 		}
 		break;
@@ -133,23 +283,27 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			break;
 		case WM_LBUTTONUP:
 			if (gDoubleClick) {
-				ShowWindow(inputHWND, SW_SHOW);
+				_SHOW(inputHWND);
+				SetFocus(inputHWND_input);
 				gDoubleClick = false;
 			}
 			break;
 		case WM_RBUTTONDOWN:
+			SetFocus(hwnd);
+
 			POINT curPoint;
 			GetCursorPos(&curPoint);
 
 			SetForegroundWindow(hwnd);
-			UINT clicked = TrackPopupMenu(gMenu, TPM_RETURNCMD | TPM_NONOTIFY, curPoint.x, curPoint.y, 0, hwnd, NULL);
+			UINT clicked = TrackPopupMenu(gTrayMenu, TPM_RETURNCMD | TPM_NONOTIFY, curPoint.x, curPoint.y, 0, hwnd, NULL);
 
 			switch (clicked) {
 			case NAM_TRAY_SHOWWINDOW:
-				ShowWindow(inputHWND, SW_SHOW);
+				_SHOW(inputHWND);
 				break;
 			case NAM_TRAY_ADDNEW:
-				ShowWindow(createNewHWND, SW_SHOW);
+				_SHOW(createNewHWND);
+				SetFocus(createNewHWND_input);
 				break;
 			case NAM_TRAY_EXIT:
 				PostQuitMessage(0);
@@ -165,8 +319,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return 0;
 }
 
+void readOldData()
+{
+	std::wifstream ifs("data.txt");
+	std::wstring name, action, input;
+	std::wstringstream wss;
+	int temp = 0;
+
+	for (std::wstring line; std::getline(ifs, line);) {
+		wss.str(line);
+		for (std::wstring entry; std::getline(wss, entry, TEXT('|'));) {
+			if (temp == 0) {
+				name = entry;
+			}
+			else if (temp == 1) {
+				action = entry;
+			}
+			else if (temp == 2) {
+				input = entry;
+			}
+
+			temp = (temp + 1) % 3;
+			if (temp == 0) {
+				gEntries.push_back(NAM_ENTRY(name, ACTION(std::stoi(action)), input));
+			}
+		}
+	}
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	readOldData();
 	gDoubleClick = false;
 	gIcon = (HICON)LoadImage(NULL, TEXT("icon.ico"), IMAGE_ICON, 0, 0, LR_LOADFROMFILE);
 
@@ -174,7 +357,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviInstance, LPSTR lpCmdLin
 
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
 	wc.hIcon = gIcon;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hInstance = hInstance;
@@ -187,13 +370,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviInstance, LPSTR lpCmdLin
 
 	// Start window
 	inputHWND = CreateWindow(TEXT("TrayBot"), TEXT("TrayBot - Input"), WS_OVERLAPPEDWINDOW, 10, 10, 310, 60, NULL, NULL, hInstance, NULL);
-	ShowWindow(inputHWND, SW_HIDE);
+	_HIDE(inputHWND);
 
-	createNewHWND = CreateWindow(TEXT("TrayBot"), TEXT("TrayBot - New Input Value"), WS_OVERLAPPEDWINDOW, 10, 10, 800, 600, NULL, NULL, hInstance, NULL);
-	ShowWindow(createNewHWND, SW_HIDE);
+	createNewHWND = CreateWindow(TEXT("TrayBot"), TEXT("TrayBot - New Input Value"), WS_OVERLAPPEDWINDOW, 10, 10, 330, 180, NULL, NULL, hInstance, NULL);
+	_HIDE(createNewHWND);
 
 	// Center the windows to the screen
 	centerWindows();
+	// Create menu for tray icon
+	gTrayMenu = CreatePopupMenu();
+
+	menuItems();
+	// Create window buttons
+	windowStyles();
 
 	// Hotkey
 	RegisterHotKey(inputHWND, NAM_HOTKEY_ID, MOD_ALT | MOD_CONTROL | MOD_SHIFT, 0x51);
@@ -204,10 +393,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviInstance, LPSTR lpCmdLin
 	gNid.uID = NAM_TRAYICON_ID;
 	gNid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
 	gNid.uCallbackMessage = NAM_TRAYICON_CBID;
+	gNid.uVersion = NOTIFYICON_VERSION_4;
 	gNid.hIcon = gIcon;
 	wcscpy_s(gNid.szTip, TEXT("TrayBot"));
 
 	Shell_NotifyIcon(NIM_ADD, &gNid);
+
+	// TODO: DELETE
+	/*
+	NAM_ENTRY ne{ TEXT("dr"), ACTION::OPENFOLDER, TEXT("C:\\Users\\TheNamlessGuy\\Dropbox") };
+	gEntries.push_back(ne);
+
+	std::get<0>(ne) = TEXT("test");
+	std::get<1>(ne) = ACTION::SETCLIPBOARD;
+	std::get<2>(ne) = TEXT("Coolboi");
+	gEntries.push_back(ne);
+	*/
 
 	// Message loop
 	MSG msg;
